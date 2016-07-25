a2be1edbfb6a03017ddb55ede4f59c5a941e639b
Caleb Gilmour <cgilmour@gmail.com>
2016-07-25 04:08:56 +0000

    More detailed event capture.

--- benchmarking/cmd/test-http-client/main.go
+++ benchmarking/cmd/test-http-client/main.go
@@ -21,6 +21,10 @@ var (
 	connectTimeout = flag.Uint("connect-timeout", 1000, "Number of milliseconds to permit connection attempts before timing out.")
 )
 
+const (
+	headerRequestID = "X-Request-ID"
+)
+
 func usage() {
 	_, cmd := path.Split(os.Args[0])
 	fmt.Fprintf(os.Stderr, "Usage: %s [-requests=n] [-connect-timeout=n] url [url...]\n", cmd)
@@ -32,6 +36,7 @@ func main() {
 
 	if len(flag.Args()) == 0 {
 		fmt.Fprintf(os.Stderr, "No URLs provided\n")
+		return
 	}
 
 	urls := make([]string, 0, len(flag.Args()))
@@ -51,35 +56,25 @@ func main() {
 		urls = append(urls, param)
 	}
 
-	defaultTransport, ok := http.DefaultTransport.(*http.Transport)
-	if !ok {
-		fmt.Fprintf(os.Stderr, "Error asserting http.DefaultTransport\n")
-		return
-	}
+	sessionMap := map[string]*session{}
+	uuidList := []string{}
+
 	bc := &byteCounter{}
-	dialer := timedDialer {
-		dial: (&net.Dialer{Timeout: time.Duration(*connectTimeout) * time.Millisecond}).Dial,
-		bc: bc,
-	}
-	defaultTransport.Dial = dialer.Dial
-	client := &http.Client{Transport: newTimedRoundTripper(http.DefaultTransport)}
+	client := &http.Client{Transport: &sessionTransport{m: sessionMap}}
 
 	numWorkers := int(*connectTimeout) / 10
 	if numWorkers < runtime.NumCPU() * 8 {
 		numWorkers = runtime.NumCPU() * 8
 	}
 	wg := &sync.WaitGroup{}
-	ch := make(chan struct{}, numWorkers)
+	ch := make(chan *session, numWorkers)
 	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			for range ch {
-				u, err := uuid.New4()
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Error creating UUID: %s\n", err)
-					continue
-				}
+			for s := range ch {
+				s.initiated = time.Now()
+				s.connections = make([]*timedConnection, 0, len(urls))
 				for _, url := range urls {
 					req, err := http.NewRequest("GET", url, nil)
 					if err != nil {
@@ -87,7 +82,7 @@ func main() {
 						continue
 					}
 					req.Close = true
-					req.Header.Set("X-Request-ID", u)
+					req.Header.Set(headerRequestID, s.uuid)
 					resp, err := client.Do(req)
 					if err != nil {
 						fmt.Fprintf(os.Stderr, "Error execuing request to '%s': %s\n", url, err)
@@ -96,6 +91,7 @@ func main() {
 					io.Copy(ioutil.Discard, resp.Body)
 					resp.Body.Close()
 				}
+				s.completed = time.Now()
 			}
 		}()
 	}
@@ -108,58 +104,82 @@ func main() {
 		}
 	}()
 
-	s := struct{}{}
 	for i := uint(0);  i < *requests; i++ {
-		before := time.Now()
-		ch <- s
-		after := time.Now()
-		if after.Sub(before) > 1 * time.Second {
-			fmt.Println("Request", i, "delay was", after.Sub(before))
+		u, err := uuid.New4()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating UUID: %s\n", err)
+			continue
 		}
+		s := newSession(u, bc)
+		sessionMap[u] = s
+		uuidList = append(uuidList, u)
+		ch <- s
 	}
 	close(ch)
 	submitFinished := time.Now()
 	fmt.Println("Submitted", *requests, "requests, waiting for completion")
 	wg.Wait()
 	fmt.Println("Waited", time.Now().Sub(submitFinished))
+	for _, v := range uuidList {
+		s := sessionMap[v]
+		queTime := s.initiated.Sub(s.generated)
+		duration := s.completed.Sub(s.initiated)
+		c := s.connections[0]
+		dialTime := c.established.Sub(c.started)
+		xferTime := c.closed.ts.Sub(c.firstWrite.ts)
+		fmt.Println(queTime, duration, dialTime, xferTime)
+	}
 }
 
-type timedRoundTripper struct {
+type session struct {
+	uuid string
 	rt http.RoundTripper
+	bc *byteCounter
+	generated time.Time
+	initiated time.Time
+	completed time.Time
+	connections []*timedConnection
 }
 
-func newTimedRoundTripper(rt http.RoundTripper) http.RoundTripper {
-	return timedRoundTripper{rt: rt}
+func newSession(uuid string, bc *byteCounter) *session {
+	s := &session{ uuid: uuid, bc: bc, generated: time.Now() }
+	s.rt = &http.Transport{Dial: s.Dial}
+	return s
 }
 
-func (t timedRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	// fmt.Println("ReqTS:", time.Now())
-	res, err := t.rt.RoundTrip(req)
-	// fmt.Println("ResTS:", time.Now())
+func (s *session) Dial(network, addr string) (net.Conn, error) {
+	tc := &timedConnection{started: time.Now()}
+	conn, err := (&net.Dialer{Timeout: time.Duration(*connectTimeout) * time.Millisecond}).Dial(network, addr)
+	tc.established = time.Now()
+	tc.bc = s.bc
+	tc.Conn = conn
+	s.connections = append(s.connections, tc)
+	return tc, err
+}
+
+type sessionTransport struct {
+	m map[string]*session
+}
+
+func (sr *sessionTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	uuid := req.Header.Get(headerRequestID)
+	sess := sr.m[uuid]
+	res, err := sess.rt.RoundTrip(req)
 	return res, err
 }
 
-type timedDialer struct {
-	dial func(network, addr string) (net.Conn, error)
-	bc *byteCounter
-}
-
-func (td timedDialer) Dial(network, addr string) (net.Conn, error) {
-	// fmt.Println("DialStart:", time.Now())
-	conn, err := td.dial(network, addr)
-	// connTime := time.Now()
-	// fmt.Println("DialEnd:  ", connTime)
-	return &timedConn{Conn: conn, openTS: time.Now(), bc: td.bc}, err
-}
-
-type timedConn struct {
+type timedConnection struct {
 	net.Conn
-	openTS time.Time
-	closeTS time.Time
+	started time.Time
+	established time.Time
+	firstRead timedEvent
+	firstWrite timedEvent
+	closed timedEvent
 	bc *byteCounter
 }
 
-func (tc *timedConn) Read(b []byte) (int, error) {
+func (tc *timedConnection) Read(b []byte) (int, error) {
+	tc.firstRead.Event()
 	n, err := tc.Conn.Read(b)
 	tc.bc.m.Lock()
 	tc.bc.rx += n
@@ -167,7 +187,8 @@ func (tc *timedConn) Read(b []byte) (int, error) {
 	return n, err
 }
 
-func (tc *timedConn) Write(b []byte) (int, error) {
+func (tc *timedConnection) Write(b []byte) (int, error) {
+	tc.firstWrite.Event()
 	n, err := tc.Conn.Write(b)
 	tc.bc.m.Lock()
 	tc.bc.tx += n
@@ -175,10 +196,33 @@ func (tc *timedConn) Write(b []byte) (int, error) {
 	return n, err
 }
 
-func (tc timedConn) Close() error {
-	tc.closeTS = time.Now()
-	// fmt.Printf("Opened: %s, Closed: %s, Delta: %s\n", tc.openTS, tc.closeTS, tc.closeTS.Sub(tc.openTS))
+func (tc *timedConnection) Close() error {
+	tc.closed.Event()
 	return tc.Conn.Close()
+}
+
+
+type timedEvent struct {
+	once sync.Once
+	occurred bool
+	ts time.Time
+}
+
+func (te *timedEvent) Event() {
+	te.once.Do(
+			func() {
+			te.ts = time.Now()
+			te.occurred = true
+			},
+		 )
+}
+
+func (te *timedEvent) When() time.Time {
+	return te.ts
+}
+
+func (te *timedEvent) Occurred() bool {
+	return te.occurred
 }
 
 type byteCounter struct {
@@ -194,4 +238,3 @@ func (bc *byteCounter) Sample() (int, int) {
 	bc.m.Unlock()
 	return rx, tx
 }
-
